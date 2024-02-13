5e26b1626e274bdff79d76bd1d020bed18873b6d
Romanov Oleksii <aleksey55121@gmail.com>
2024-02-13 16:08:43 +0100

    feat: add Hint component

---
+++ components/Hint.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export interface IHintProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+  alignOffset?: number;
+}
+export const Hint: React.FC<IHintProps> = ({
+  label,
+  align,
+  alignOffset,
+  sideOffset,
+  side,
+  children,
+}) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent
+          className={"text-white bg-black border-black"}
+          side={side}
+          align={align}
+          alignOffset={alignOffset}
+          sideOffset={sideOffset}
+        >
+          <p className={"font-semibold capitalize"}>{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
