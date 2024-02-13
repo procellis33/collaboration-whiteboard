8c15485ba2b31aa81c9c8dd6cc24f04ddd8ea31a
Romanov Oleksii <aleksey55121@gmail.com>
2024-02-13 16:10:53 +0100

    refactor: new layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Index from "@/app/(dashboard)/_components/sidebar";
+import OrganisationSidebar from "@/app/(dashboard)/_components/OrganisationSidebar";
+import Navbar from "@/app/(dashboard)/_components/Navbar";
+
+interface IDashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout: React.FC<IDashboardLayoutProps> = ({ children }) => {
+  return (
+    <main className={"h-full"}>
+      <Index />
+      <div className={"pl-[60px] h-full"}>
+        <div className={"flex gap-x-3 h-full"}>
+          <OrganisationSidebar />
+          <div className={"h-full flex-1"}>
+            <Navbar />
+            {children}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default DashboardLayout;
