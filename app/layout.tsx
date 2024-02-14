2c0407c25c1e6212eeee17edcae3bb88e47c0700
Romanov Oleksii <aleksey55121@gmail.com>
2024-02-14 22:47:25 +0100

    refactor: add toaster to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import { ConvexClientProvider } from "@/providers/convex-client-provider";
+import { ConvexClientProvider } from "@/providers/convexClientProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider>
+          <Toaster />
+          {children}
+        </ConvexClientProvider>
       </body>
     </html>
   );
