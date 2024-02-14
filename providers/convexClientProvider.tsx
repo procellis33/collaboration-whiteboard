7997e2ffc43dd2baa5f3bbe0bf52b26302803202
Romanov Oleksii <aleksey55121@gmail.com>
2024-02-14 22:39:53 +0100

    chore: renaming file

---
+++ providers/convexClientProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import React from "react";
+import { Loading } from "@/components/auth/loading";
+
+interface IConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider: React.FC<IConvexClientProviderProps> = ({
+  children,
+}) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <Authenticated>{children}</Authenticated>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
