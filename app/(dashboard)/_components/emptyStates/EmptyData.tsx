b88c5e16f7a7132070133c218604f2ddd579d9b1
Romanov Oleksii <aleksey55121@gmail.com>
2024-02-14 22:53:00 +0100

    feat: add button to create new board from empty state screen

--- app/(dashboard)/_components/emptyStates/EmptyData.tsx
+++ app/(dashboard)/_components/emptyStates/EmptyData.tsx
@@ -1,14 +1,36 @@
 import { Button } from "@/components/ui/button";
 import { EmptyState } from "@/app/(dashboard)/_components/emptyStates/EmptyState";
+import { api } from "@/convex/_generated/api";
+import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/useApiMutation";
+import { toast } from "sonner";
 
 export const EmptyData = () => {
+  const { organization } = useOrganization();
+  const { pending, mutate } = useApiMutation(api.board.create);
+
+  const handleClick = () => {
+    if (!organization) return;
+    mutate({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+      .then(() => toast.success("Board created"))
+      .catch((e) => toast.error(e.message));
+  };
+
   return (
     <EmptyState
       header={"Create your first board"}
       subheader={"Start by creating a board for your organization"}
       imageSrc={"/emptyData.svg"}
     >
-      <Button className={"mt-6"} size={"lg"}>
+      <Button
+        disabled={pending}
+        onClick={handleClick}
+        className={"mt-6"}
+        size={"lg"}
+      >
         Create board
       </Button>
     </EmptyState>
