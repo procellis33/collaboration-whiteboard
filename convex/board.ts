24411b7c73085f4246664081113671542dfb5cac
Romanov Oleksii <aleksey55121@gmail.com>
2024-02-16 16:49:59 +0100

    feat: add title update mutation

--- convex/board.ts
+++ convex/board.ts
@@ -44,3 +44,32 @@ export const create = mutation({
     });
   },
 });
+
+export const remove = mutation({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Unauthorized");
+
+    return await ctx.db.delete(args.id);
+  },
+});
+
+export const update = mutation({
+  args: { id: v.id("boards"), title: v.string() },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Unauthorized");
+
+    const title = args.title.trim();
+
+    if (!title) throw new Error("Title is required");
+
+    if (title.length > 60)
+      throw new Error("Title cannot be longer than 60 characters");
+
+    return await ctx.db.patch(args.id, {
+      title: args.title,
+    });
+  },
+});
