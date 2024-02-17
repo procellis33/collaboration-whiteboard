ff08217933b74fc96c75f5bc1dd5832e0d32a47a
Romanov Oleksii <aleksey55121@gmail.com>
2024-02-17 01:04:49 +0100

    feat: add fetch favorite boards

--- convex/boards.ts
+++ convex/boards.ts
@@ -1,13 +1,37 @@
 import { v } from "convex/values";
 import { query } from "./_generated/server";
+import { getAllOrThrow } from "convex-helpers/server/relationships";
+import { Id } from "./_generated/dataModel";
 
 export const get = query({
   args: {
     orgId: v.string(),
+    favorites: v.optional(v.boolean()),
   },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) throw new Error("Unauthorized");
+
+    if (args.favorites) {
+      const favoriteBoards = await ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_org", (q) =>
+          q.eq("userId", identity.subject).eq("orgId", args.orgId),
+        )
+        .order("desc")
+        .collect();
+
+      const ids: Id<"boards">[] = favoriteBoards.map(
+        (board) => board.boardId as Id<"boards">,
+      );
+
+      const boards = await getAllOrThrow(ctx.db, ids);
+
+      return boards.map((board) => ({
+        ...board,
+        isFavorite: true,
+      }));
+    }
 
     const boards = await ctx.db
       .query("boards")
