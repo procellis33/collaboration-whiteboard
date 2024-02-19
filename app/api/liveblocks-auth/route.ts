4c249a9b7831235a2e3c3e2e0f473296ab411ab0
Romanov Oleksii <aleksey55121@gmail.com>
2024-02-20 00:57:15 +0100

    feat: add auth check endpoint

---
+++ app/api/liveblocks-auth/route.ts
@@ -0,0 +1,35 @@
+import { Liveblocks } from "@liveblocks/node";
+import { ConvexHttpClient } from "convex/browser";
+import { auth, currentUser } from "@clerk/nextjs";
+import { api } from "@/convex/_generated/api";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+const liveblocks = new Liveblocks({
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!,
+});
+
+export const POST = async (request: Request) => {
+  const authorization = auth();
+  const user = await currentUser();
+
+  if (!authorization || !user)
+    return new Response("Unauthorized", { status: 403 });
+
+  const { room } = await request.json();
+  const board = await convex.query(api.board.get, { id: room });
+
+  if (board?.orgId !== authorization.orgId)
+    return new Response("Unauthorized", { status: 403 });
+
+  const userInfo = {
+    name: user.firstName || "Anonymous",
+    picture: user.imageUrl,
+  };
+
+  const session = liveblocks.prepareSession(user.id, { userInfo: userInfo });
+
+  if (room) session.allow(room, session.FULL_ACCESS);
+  const { status, body } = await session.authorize();
+  return new Response(body, { status });
+};
