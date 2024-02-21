b66a415385453dbca7dc20f549271361fe0ea65d
Romanov Oleksii <aleksey55121@gmail.com>
2024-02-21 02:00:37 +0100

    feat: toolbar with canvas state

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -1,22 +1,35 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Info } from "@/app/board/[boardId]/_components/Info";
 import { Participants } from "@/app/board/[boardId]/_components/Participants";
 import { Toolbar } from "@/app/board/[boardId]/_components/Toolbar";
-import { useSelf } from "@/liveblocks.config";
+import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
+import { ECanvasMode, TCanvasState } from "@/types/canvas";
 
 interface ICanvasProps {
   boardId: string;
 }
 
 export const Canvas: React.FC<ICanvasProps> = ({ boardId }) => {
-  const info = useSelf((me) => me.info);
+  const [canvasState, setCanvasState] = useState<TCanvasState>({
+    mode: ECanvasMode.None,
+  });
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
   return (
     <main className={"h-full w-full relative bg-neutral-100 touch-none"}>
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
