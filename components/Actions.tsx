fd382b6f02189094c8427845526b29c785a3f3ce
Romanov Oleksii <aleksey55121@gmail.com>
2024-03-05 01:27:49 +0100

    feat: add action's mode and theme

--- components/Actions.tsx
+++ components/Actions.tsx
@@ -7,7 +7,15 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Link2, Pencil, Trash2 } from "lucide-react";
+import {
+  Link2,
+  Pencil,
+  Trash2,
+  Github,
+  Briefcase,
+  Moon,
+  Sun,
+} from "lucide-react";
 import { toast } from "sonner";
 import { useApiMutation } from "@/hooks/useApiMutation";
 import { api } from "@/convex/_generated/api";
@@ -39,6 +47,7 @@ interface IActionProps {
   sideOffset?: DropdownMenuContentProps["sideOffset"];
   redirectHomeOnDelete?: boolean;
   className?: string;
+  mode?: EMode;
 }
 
 export const Actions: React.FC<IActionProps> = ({
@@ -49,10 +58,12 @@ export const Actions: React.FC<IActionProps> = ({
   title,
   redirectHomeOnDelete,
   className,
+  mode,
 }) => {
   const { pending, mutate } = useApiMutation(api.board.remove);
   const { onOpen } = useRenameModal();
   const router = useRouter();
+  const { theme, setTheme } = useTheme();
 
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const handleDialogItemOpenChange = (open: boolean) => {
@@ -82,12 +93,34 @@ export const Actions: React.FC<IActionProps> = ({
         sideOffset={sideOffset}
         side={side}
         onClick={(e) => e.stopPropagation()}
-        className={twMerge("w-60", className)}
+        className={twMerge("w-60 dark:bg-zinc-800", className)}
       >
-        <DropdownMenuItem className={"p-3 cursor-pointer"} onClick={onCopyLink}>
-          <Link2 className={"h-4 w-4 mr-2"} />
-          Copy board link
-        </DropdownMenuItem>
+        {mode === EMode.Canvas && (
+          <DropdownMenuItem
+            className={"p-3 cursor-pointer"}
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            <Image
+              src={"/logo.svg"}
+              alt={"logo"}
+              width={16}
+              height={16}
+              className={"h-4 w-4 mr-2"}
+            />
+            Home
+          </DropdownMenuItem>
+        )}
+        {mode !== EMode.Canvas && (
+          <DropdownMenuItem
+            className={"p-3 cursor-pointer"}
+            onClick={onCopyLink}
+          >
+            <Link2 className={"h-4 w-4 mr-2"} />
+            Copy board link
+          </DropdownMenuItem>
+        )}
         <DropdownMenuItem
           className={"p-3 cursor-pointer"}
           onClick={() => onOpen(id, title)}
@@ -103,13 +136,55 @@ export const Actions: React.FC<IActionProps> = ({
           onConfirm={onDelete}
         >
           <Button
-            variant="ghost"
-            className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+            variant="ghostCanvas"
+            className="p-3 h-11 cursor-pointer text-sm w-full justify-start font-normal"
           >
             <Trash2 className={"h-4 w-4 mr-2"} />
             Delete
           </Button>
         </ConfirmModal>
+        {mode === EMode.Canvas && (
+          <>
+            <div className={"px-3 my-2 w-full h-1.5 bg-slate-400/10"} />
+            <DropdownMenuItem className={"p-3 cursor-pointer"}>
+              <a
+                className={
+                  "flex items-center text-primary underline-offset-4 hover:underline"
+                }
+                href={GITHUB_LINK}
+              >
+                <Github className={"h-4 w-4 mr-2"} />
+                GitHub
+              </a>
+            </DropdownMenuItem>
+            <DropdownMenuItem className={"p-3 cursor-pointer"}>
+              <a
+                className={
+                  "flex items-center text-primary underline-offset-4 hover:underline"
+                }
+                href={PORTFOLIO_LINK}
+              >
+                <Briefcase className={"h-4 w-4 mr-2"} />
+                Portfolio
+              </a>
+            </DropdownMenuItem>
+            <div className={"px-3 my-2 w-full h-1.5 bg-slate-400/10"} />
+            <DropdownMenuItem
+              className={"p-3 cursor-pointer capitalize"}
+              onClick={() => {
+                const switchThemeTo = theme === "dark" ? "light" : "dark";
+                setTheme(switchThemeTo);
+              }}
+            >
+              {theme === "dark" ? (
+                <Moon className={"h-4 w-4 mr-2"} />
+              ) : (
+                <Sun className={"h-4 w-4 mr-2"} />
+              )}
+              {theme}
+            </DropdownMenuItem>
+          </>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
