fe8221a1ca8a6edfc531abe1224ed6306cf047f6
Romanov Oleksii <aleksey55121@gmail.com>
2024-03-05 01:38:35 +0100

    feat: add shortcut to hint

--- components/Hint.tsx
+++ components/Hint.tsx
@@ -5,14 +5,17 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { twMerge } from "tailwind-merge";
 
 export interface IHintProps {
   label: string;
   children: React.ReactNode;
+  shortcut?: string;
   side?: "top" | "bottom" | "left" | "right";
   align?: "start" | "center" | "end";
   sideOffset?: number;
   alignOffset?: number;
+  className?: string;
 }
 export const Hint: React.FC<IHintProps> = ({
   label,
@@ -21,19 +24,28 @@ export const Hint: React.FC<IHintProps> = ({
   sideOffset,
   side,
   children,
+  className,
+  shortcut,
 }) => {
   return (
     <TooltipProvider>
       <Tooltip delayDuration={100}>
         <TooltipTrigger asChild>{children}</TooltipTrigger>
         <TooltipContent
-          className={"text-white bg-black border-black"}
+          className={twMerge("text-white bg-black border-black", className)}
           side={side}
           align={align}
           alignOffset={alignOffset}
           sideOffset={sideOffset}
         >
-          <p className={"font-semibold"}>{label}</p>
+          <p className={"font-semibold"}>
+            {label}
+            {shortcut && (
+              <span className={"text-xs ml-1 text-muted-foreground"}>
+                ({shortcut})
+              </span>
+            )}
+          </p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
